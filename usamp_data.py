4459024db80f5fd2d1636ce1ed47e96a2469d7fc
Bhavya Vasudeva <30321205+estija@users.noreply.github.com>
2021-01-06 01:47:21 +0530

    add fastmri

--- usamp_data.py
+++ usamp_data.py
@@ -5,26 +5,28 @@ import numpy as np
 import cmath
 
 
-def usam_data(accel, data_tensor, mask):
+def usam_data(data_tensor, mask, dataset):
     data =[] 
     for i in range(data_tensor.shape[0]):
-    	fourier = np.fft.fft2(data_tensor[i,:,:])
-    	cen_fourier  = np.fft.fftshift(fourier)
-    	subsam_fourier = np.multiply(cen_fourier,mask) #undersampling in k-space
-    	uncen_fourier = np.fft.ifftshift(subsam_fourier)
-    	zro_image = np.fft.ifft2(uncen_fourier) #zero-filled reconstruction
-    	data.append(zro_image)        
+        fourier = np.fft.fft2(data_tensor[i,:,:])
+        if dataset=='miccai' or dataset=='mrnet':
+          fourier  = np.fft.fftshift(fourier)
+        subsam_fourier = np.multiply(fourier,mask) #undersampling in k-space
+        uncen_fourier = np.fft.ifftshift(subsam_fourier)
+        zro_image = np.fft.ifft2(uncen_fourier) #zero-filled reconstruction
+        data.append(zro_image)        
     data = np.asarray(data)
     return data 
 
 
-def usam_data_noise(accel, data_tensor, mask, noise_ratio):
+def usam_data_noise(data_tensor, mask, noise_ratio, dataset):
     fft_data=[]
     data =[]
     for i in range(data_tensor.shape[0]):
         fourier = np.fft.fft2(data_tensor[i,:,:])
-        cen_fourier  = np.fft.fftshift(fourier)
-        fft_data.append(cen_fourier)
+        if dataset=='miccai' or dataset=='mrnet':
+          fourier  = np.fft.fftshift(fourier)
+        fft_data.append(fourier)
     fft_data=np.asarray(fft_data)
     fft_std=np.std(fft_data)
     nstd=(noise_ratio*fft_std)/np.sqrt(2)
@@ -39,24 +41,30 @@ def usam_data_noise(accel, data_tensor, mask, noise_ratio):
     data = np.asarray(data)
     return data 
 
+dataset='miccai' #dataset to be used: miccai or mrnet or fastmri
 save_path='/home/Co-VeGAN/'
 mask_path='/home/Co-VeGAN/masks/mask_1dg_a5.pickle' #path for the required mask
 maf=open(mask_path,'rb')
 mask=pickle.load(maf)
 
-accel=5 #acceleration factor
 
 #creating undersampled training data
 train_path='/home/Co-VeGAN/training_gt_aug.pickle'
 trf=open(train_path,'rb')
 train_data=pickle.load(trf)
 
-#for mrnet, replace the indices with the commented ones
-train_data_new = usam_data(accel, train_data[0:13461,:,:], mask)                  #0:8100       #nonoise
-train_data_new2 = usam_data_noise(accel, train_data[13461:13956,:,:], mask, 0.1)  #8100:8400    #10%noise-overlapping
-train_data_new3 = usam_data_noise(accel, train_data[13956:14451,:,:], mask, 0.2)  #8400:8700    #20%noise-overlapping
-train_data_new4 = usam_data_noise(accel, train_data[14451:17619,:,:,], mask, 0.1) #8700:10600   #10%noise-nonoverlapping
-train_data_new5 = usam_data_noise(accel, train_data[17619:20787,:,:], mask, 0.2)  #10600:12500  #20%noise-nonoverlapping
+if dataset=='miccai':
+  train_data_new = usam_data(train_data[0:13461,:,:], mask, dataset)                  #no noise
+  train_data_new2 = usam_data_noise(train_data[13461:13956,:,:], mask, 0.1, dataset)  #10% noise-overlapping
+  train_data_new3 = usam_data_noise(train_data[13956:14451,:,:], mask, 0.2, dataset)  #20% noise-overlapping
+  train_data_new4 = usam_data_noise(train_data[14451:17619,:,:,], mask, 0.1, dataset) #10% noise-nonoverlapping
+  train_data_new5 = usam_data_noise(train_data[17619:20787,:,:], mask, 0.2, dataset)  #20% noise-nonoverlapping
+else:
+  train_data_new = usam_data(train_data[0:8100,:,:], mask, dataset)                  #no noise
+  train_data_new2 = usam_data_noise(train_data[8100:8400,:,:], mask, 0.1, dataset)  #10% noise-overlapping
+  train_data_new3 = usam_data_noise(train_data[8400:8700,:,:], mask, 0.2, dataset)  #20% noise-overlapping
+  train_data_new4 = usam_data_noise(train_data[8700:10600,:,:,], mask, 0.1, dataset) #10% noise-nonoverlapping
+  train_data_new5 = usam_data_noise(train_data[10600:12500,:,:], mask, 0.2, dataset)  #20% noise-nonoverlapping
 
 stack1 = np.vstack((train_data_new,train_data_new2))
 stack2 = np.vstack((train_data_new3, train_data_new4))
@@ -64,7 +72,7 @@ stack3 = np.vstack((stack2, train_data_new5))
 fstack = np.vstack((stack1, stack3))
 
 with open(os.path.join(save_path,'training_usamp_1dg_a5_aug.pickle'),'wb') as f:
-	pickle.dump(fstack,f,protocol=4)
+    pickle.dump(fstack,f,protocol=4)
 
 '''
 #creating undersampled testing data
@@ -72,9 +80,9 @@ test_path='/home/Co-VeGAN/testing_gt.pickle'
 tef=open(test_path,'rb')
 test_data=pickle.load(tef)
 
-train_data_new = usam_data(accel, test_data,mask) #for noise-free imgs
-#test_data_new=usam_data_noise(accel,test_data,mask,0.1) #for imgs with 10%noise
-#test_data_new=usam_data_noise(accel,test_data,mask,0.2) #for imgs with 20%noise
+test_data_new = usam_data(test_data,mask,dataset) #for noise-free imgs
+#test_data_new=usam_data_noise(test_data,mask,0.1,dataset) #for imgs with 10% noise
+#test_data_new=usam_data_noise(test_data,mask,0.2,dataset) #for imgs with 20% noise
 with open(os.path.join(save_path,'testing_usamp_1dg_a5.pickle'),'wb') as f:
-	pickle.dump(test_data_new,f,protocol=4)
+    pickle.dump(test_data_new,f,protocol=4)
 '''
