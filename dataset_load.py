d4c40e682cf4bd18952dc97d8bcf706bc455a54c
Bhavya Vasudeva <30321205+estija@users.noreply.github.com>
2021-01-06 01:30:42 +0530

    add fastmri

--- dataset_load.py
+++ dataset_load.py
@@ -2,8 +2,10 @@ import os
 import nibabel as nib 
 import matplotlib.pyplot as plt
 import numpy as np
+import h5py
 import pickle
 
+dataset='miccai' #dataset to be used: miccai or mrnet or fastmri
 save_path='/home/Co-VeGAN/'
 
 def load_a(path, num):
@@ -43,15 +45,58 @@ def load_b(path):
     data = np.asarray(data)
     return data
 
-def train_data_aug(train_gt):
-    #for mrnet, replace the indices with the commented ones
-    gt1=train_gt[0:13461,:,:]       #0:8100
+def load_c(path,num,mode):
+    fl = os.listdir(path)
+    data = []
+    #use imgs with more than 10% non-zero values
+    n_zero_ratio = 0.1
+    k = 0
+    for i in range(num):
+      if mode=='train':
+        filename = os.path.join(path, fl[i])
+      else:
+        filename = os.path.join(path, fl[i+700]) 
+      f = h5py.File(filename,'r')
+      data_new = f['kspace']
+      data_new = np.asarray(data_new)
+      for j in range(data_new[0]):
+        #ifft
+        knee = np.fft.fftshift(np.fft.ifft2(data_new[j,:,:]))
+        #crop 320x320 from centre
+        knee = knee[data_new.shape[1]//2-160:data_new.shape[1]//2+160, data_new.shape[2]//2-160:data_new.shape[2]//2+160]
+        data.append(knee)
+      s = k
+      vol = np.asarray(data)
+      k = vol.shape[0]
+      maxr = np.max(np.real(vol[s:k]))
+      maxi = np.max(np.imag(vol[s:k]))
+      minr = np.min(np.real(vol[s:k]))
+      mini = np.min(np.imag(vol[s:k]))
+      vol[s:k] = vol[s:k]/np.max([maxr,maxi,np.abs(minr),np.abs(mini)])
+      data = list(vol)
 
-    gt2a=train_gt[12471:12966,:,:]  #7500:7800    #overlapping data
-    gt2b=train_gt[12966:13461,:,:]  #7800:8100    #overlapping data
+    data = np.asarray(data)
+    return data
 
-    gt3a=train_gt[13461:16629,:,:]  #8100:10000   #non-overlapping data
-    gt3b=train_gt[16629:19797,:,:]  #10000:11900  #non-overlapping data
+
+def train_data_aug(train_gt,dataset):
+    if dataset=='miccai':
+        gt1=train_gt[0:13461,:,:]
+
+        gt2a=train_gt[12471:12966,:,:]  #overlapping data
+        gt2b=train_gt[12966:13461,:,:]  #overlapping data
+
+        gt3a=train_gt[13461:16629,:,:]  #non-overlapping data
+        gt3b=train_gt[16629:19797,:,:]  #non-overlapping data
+
+    else:
+        gt1=train_gt[0:8100,:,:]
+
+        gt2a=train_gt[7500:7800,:,:]  #overlapping data
+        gt2b=train_gt[7800:8100,:,:]  #overlapping data
+
+        gt3a=train_gt[8100:10000,:,:]  #non-overlapping data
+        gt3b=train_gt[10000:11900,:,:]  #non-overlapping data
 
     gt2=np.vstack((gt2a,gt2b))
     gt3=np.vstack((gt3a,gt3b))
@@ -64,30 +109,33 @@ def train_data_aug(train_gt):
 
 #for training data
 
-#miccai dataset
-train_path='/home/Co-VeGAN/training-training/warped-images'
-train_gt=load_a(train_path,1090)
+if dataset=='miccai':
+  train_path='/home/Co-VeGAN/training-training/warped-images'
+  train_gt=load_a(train_path,1090)
+elif dataset=='mrnet':
+  train_path='/home/Co-VeGAN/train/coronal'
+  train_gt=load_b(train_path)
+elif dataset=='fastmri':
+  train_path='/home/Co-VeGAN/singlecoil_train'
+  train_gt=load_c(train_path,450,'train')
 
-#mrnet dataset
-#train_path='/home/Co-VeGAN/train/coronal'
-#train_gt=load_b(train_path)
-
-train_gt_aug=train_data_aug(train_gt) #created gt for augmented data
+train_gt_aug=train_data_aug(train_gt,dataset) #created gt for augmented data
 
 with open(os.path.join(save_path,'training_gt_aug.pickle'),'wb') as f:
         pickle.dump(train_gt_aug,f,protocol=4)
 
-
 '''
 #for testing data
 
-#miccai dataset
-test_path='/home/Co-VeGAN/training-testing/warped-images'
-test_data=load_a(test_path, 390)
-
-#mrnet dataset
-#test_path='/home/Co-VeGAN/valid/coronal'
-#test_data=load_b(test_path)
+if dataset=='miccai':
+  test_path='/home/Co-VeGAN/training-testing/warped-images'
+  test_data=load_a(test_path, 390)
+elif dataset=='mrnet':
+  test_path='/home/Co-VeGAN/valid/coronal'
+  test_data=load_b(test_path)
+elif dataset=='fastmri':
+  test_path='/home/Co-VeGAN/singlecoil_train'
+  test_data=load_c(test_path, 100, 'test')
 
 with open(os.path.join(save_path,'testing_gt.pickle'),'wb') as f:
        pickle.dump(test_data,f,protocol=4)
